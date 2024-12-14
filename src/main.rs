a42448a1b5e3c9d01f2cfa98dd23f096b12d2508
Gagandeep Thapar <gsthapar@gmail.com>
2024-12-13 19:56:22 -0800

    updating main with args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use utils::{AOCChallenge, AOCResult};
+use clap::Parser;
+use utils::AOCResult;
 pub mod aoc_utils;
 pub mod r2022;
 pub mod r2023;
@@ -6,28 +7,14 @@ pub mod r2024;
 pub mod utils;
 
 // INPUTS
-const YEAR: &str = "2024";
-const DAY: &str = "07";
-pub use r2024::day07 as today;
-
-// FILES
-const TEST1_FILE: &str = const_format::concatcp!("TESTS/", YEAR, "/day", DAY, "/part_1.txt");
-const TEST2_FILE: &str = const_format::concatcp!("TESTS/", YEAR, "/day", DAY, "/part_2.txt");
-const TEST_SOLN: &str = const_format::concatcp!("TESTS/", YEAR, "/day", DAY, "/test_solns.txt");
-const INPT_FILE: &str = const_format::concatcp!("INPUTS/", YEAR, "/day", DAY, ".txt");
-
-// SOLUTIONS
-const TEST1_SOLN: Option<usize> = None;
-const TEST2_SOLN: Option<usize> = None;
+pub use r2024::day13 as today;
 
 fn main() -> AOCResult<()> {
-    let aoc = AOCChallenge::new(TEST1_FILE, TEST2_FILE, INPT_FILE, TEST_SOLN);
+    // Parse Arguments
+    let aoc_args = utils::AOCArgs::parse();
 
-    // PART 1 //
-    let _ = aoc.part1(TEST1_SOLN)?;
-
-    // PART 2 //
-    let _ = aoc.part2(TEST2_SOLN)?;
+    // Process Arguments
+    aoc_args.process()?;
 
     // END //
     Ok(())
